e69520725e4c02d539b1bdc3b0502ccbf6c85e36
haloworker <haloworker@111.com>
2024-08-21 22:55:20 +0800

    feat(scripts): 新增创建Django App的脚本并放置在apps目录中
    - 添加了用于创建Django App的脚本
    - 自动将新创建的App放置在apps目录中
    - 简化了Django项目中App的创建流程

--- scripts/create_app.py
+++ scripts/create_app.py
@@ -1,101 +1,170 @@
 import os
 import logging
-
-from django.core.management.base import BaseCommand
+from django.conf import settings
 from application.settings import BASE_DIR
 
 logger = logging.getLogger(__name__)
 
 
-class Command(BaseCommand):
+class DjangoAppCreator:
     """
-    向apps目录中创建App命令:
-    python manage.py startapp app名
-    python manage.py startapp app名1 app名2
+    创建Django App，将其放在apps目录中
     """
 
-    def add_arguments(self, parser):
-        parser.add_argument('appname', nargs='*', type=str, )
+    def __init__(self, app_name):
+        """
+        :param app_name: App名称
+        """
+        self.app_name = app_name
+        self.app_path = os.path.join(settings.BASE_DIR, "apps", app_name)
 
-    def handle(self, *args, **options):
-        appname_list = options.get('appname')
-        if appname_list:
-            for name in appname_list:
-                app_path = os.path.join(BASE_DIR, "apps", name)
-                # 判断app是否存在
-                if os.path.exists(app_path):
-                    print(f"要创建的App名称：{name} 已存在，请更换其他名称！")
-                else:
-                    # 创建app文件夹
-                    if not os.path.exists(app_path):
-                        # 如果目标路径不存在就创建
-                        os.makedirs(app_path)
-                    # 创建__init__.py文件
-                    with open(os.path.join(app_path, "__init__.py"), 'a', encoding='UTF-8') as f:
-                        pass
-                    migrations_path = os.path.join(app_path, "migrations")
-                    # 创建migrations目录和__init__.py文件
-                    if not os.path.exists(migrations_path):
-                        # 如果目标路径不存在就创建
-                        os.makedirs(migrations_path)
-                    with open(os.path.join(migrations_path, "__init__.py"), 'a', encoding='UTF-8') as f:
-                        pass
-                    # 创建apps.py文件
-                    app_init_content = f"""from django.apps import AppConfig
+    def create_app(self):
+        """
+        创建App
+        """
+        if os.path.exists(self.app_path):
+            print(self.warning(f"App name: {self.app_name} already exists. Please choose another name!"))
+            return False
 
-class {name.capitalize()}Config(AppConfig):
+        os.makedirs(self.app_path)
+        self._create_init_file()
+        self._create_migrations_directory()
+        self._create_app_files()
+        print(self.success(f"App name: {self.app_name} created successfully in the apps directory!"))
+        return True
+
+    @staticmethod
+    def warning(message):
+        """
+        :param message: 警告信息
+        """
+        return f"\033[1;33m[WARNING] {message}\033[0m"
+
+    @staticmethod
+    def success(message):
+        """
+        :param message: 成功信息
+        """
+        return f"\033[1;32m{message}\033[0m"
+
+    def _create_init_file(self):
+        """
+        创建__init__.py文件
+        """
+        open(os.path.join(self.app_path, "__init__.py"), 'a', encoding='UTF-8').close()
+
+    def _create_migrations_directory(self):
+        """
+        创建migrations目录和__init__.py文件
+        """
+        migrations_path = os.path.join(self.app_path, "migrations")
+        os.makedirs(migrations_path)
+        open(os.path.join(migrations_path, "__init__.py"), 'a', encoding='UTF-8').close()
+
+    def _create_app_files(self):
+        """
+        创建App文件
+        """
+        files_content = {
+            "apps.py": self._get_apps_content(),
+            "models.py": self._get_models_content(),
+            "views.py": self._get_views_content(),
+            "urls.py": self._get_urls_content(),
+            "admin.py": self._get_admin_content(),
+            "tests.py": self._get_tests_content(),
+            "serializers.py": self._get_serializers_content(),
+            "filters.py": self._get_filters_content()
+        }
+
+        for file_name, content in files_content.items():
+            with open(os.path.join(self.app_path, file_name), 'w', encoding='UTF-8') as f:
+                f.write(content)
+
+    def _get_apps_content(self):
+        return f"""from django.apps import AppConfig
+
+
+class {self.app_name.capitalize()}Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = 'apps.{name}'
+    name = 'apps.{self.app_name}'
+
+
 """
-                    with open(os.path.join(app_path, "apps.py"), 'w', encoding='UTF-8') as f:
-                        f.write(app_init_content)
-                    # 创建models.py文件
-                    model_init_content = f"""from django.db import models
-from utils.models import CoreModel,BaseModel
+
+    def _get_models_content(self):
+        return """from django.db import models
+from configs.config import table_prefix
+from utils.models import CoreModel, BaseModel
 """
-                    with open(os.path.join(app_path, "models.py"), 'a', encoding='UTF-8') as f:
-                        f.write(model_init_content)
-                    # 创建views.py文件
-                    view_init_content = f"""from rest_framework.views import APIView
-from apps.lymessages.models import MyMessageTemplate,MyMessage,MyMessageUser
-from utils.jsonResponse import SuccessResponse,ErrorResponse,DetailResponse
-from utils.common import get_parameter_dic,formatdatetime
-from django.db.models import Q,F,Sum,Count
+
+    def _get_views_content(self):
+        return """from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from utils.validator import handle_serializer_validation
+from utils.json_response import SuccessResponse, ErrorResponse, DetailResponse
+from utils.common import get_parameter_dict, formatdatetime
+from django.db.models import Q, F, Sum, Count
 from django.db import transaction
 from rest_framework import serializers
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from utils.serializers import CustomModelSerializer
-from utils.viewset import CustomModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from utils.pagination import CustomPagination
 import logging
+
 logger = logging.getLogger(__name__)
+
+# Create your views here.
 """
-                    with open(os.path.join(app_path, "views.py"), 'a', encoding='UTF-8') as f:
-                        f.write(view_init_content)
-                    # 创建urls.py文件
-                    urls_init_content = f"""# -*- coding: utf-8 -*-
 
-#路由文件
-
-from django.urls import path, re_path
+    def _get_urls_content(self):
+        return """from django.urls import path, re_path
 from rest_framework import routers
 
-#from apps.lymonitor.views import MonitorManageViewSet
-
-system_url = routers.SimpleRouter()
-#system_url.register(r'monitor', MonitorManageViewSet)
+url = routers.SimpleRouter()
+# url.register(r'some_view', SomeViewSet)
 
 urlpatterns = [
-#    path('getsysteminfo/',MonitorManageViewSet.as_view({{'get':'getsysteminfo'}}), name='startapp创建app路由文件名'),
+    # path('some_path/', SomeView.as_view(), name='some_name'),
 ]
-urlpatterns += system_url.urls
+
+urlpatterns += url.urls
 """
-                    with open(os.path.join(app_path, "urls.py"), 'a', encoding='UTF-8') as f:
-                        f.write(urls_init_content)
 
-                    print(f"App名称：{name} 创建成功！")
+    def _get_admin_content(self):
+        return """from django.contrib import admin
 
-        else:
-            print(f"提示：该命令会把app创建到apps目录中")
-            print(f"请输入要创建的app名称如：python manage.py create_app app名")
+# Register your models here.
+"""
+
+    def _get_tests_content(self):
+        return """from django.test import TestCase
+
+# Create your tests here.
+"""
+
+    def _get_serializers_content(self):
+        return """from rest_framework import serializers
+from .models import *
+
+# Create your serializers here.
+"""
+
+    def _get_filters_content(self):
+        return """import django_filters
+from django_filters.rest_framework import FilterSet
+from .models import *
+
+# Create your filters here.
+"""
+
+
+def run(*args):
+    if not args:
+        print(DjangoAppCreator.warning(
+            "Usage: python manage.py runscript create_app --script-args app_name1 [app_name2 ...]"))
+        print("This script will create the app(s) in the 'apps' directory of your Django project.")
+        return
+
+    for app_name in args:
+        creator = DjangoAppCreator(app_name)
+        creator.create_app()
